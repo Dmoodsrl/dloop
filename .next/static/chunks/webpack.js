69ff749cd534f8ac83a35f5fe6e36b6d5d75e9c4
Dmoodsrl <info@developermood.com>
2025-04-05 20:26:26 +0200

    DLOOP-2-scocca-dashboard

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bcfa0c2cc7f5ec88"; }
+/******/ 		__webpack_require__.h = function() { return "afd847d3b702a9f6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
